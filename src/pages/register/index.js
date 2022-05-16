5f75ad773943424e54d3761d790f339a6a030e2a
LeHuuVu <89371606+LeHuuVu@users.noreply.github.com>
2022-05-17 00:17:11 +0700

    none

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -85,130 +85,137 @@ function Register() {
   );
 
   return (
-    <div>
-      <Form {...formItemLayout}
-        form={form}
-        name="register"
-        onFinish={onFinish}
-        initialValues={{
-          residence: ['zhejiang', 'hangzhou', 'xihu'],
-          prefix: '86',
-        }}
-        scrollToFirstError
-      >
-        <Form.Item label="Seller" valuePropName="checked">
-          <Switch />
-        </Form.Item>
-
-        <Form.Item
-          name="email"
-          label="E-mail"
-          rules={[
-            {
-              type: 'email',
-              message: 'The input is not valid E-mail!',
-            },
-            {
-              required: true,
-              message: 'Please input your E-mail!',
-            },
-          ]}
+    <div >
+      <div style={{width:"50%", float:"left", textAlign:"right"}}>
+        <img src="E:\Benkyou\Nam_III_KY_II\PBL5\pbl5-frontend\src\logo_app.svg" alt='E-commerce platform' width={600}/>
+      </div>
+      <div style={{width:"50%", float:"right"}}>
+        <Form {...formItemLayout}
+          form={form}
+          name="register"
+          className='div_border'
+          onFinish={onFinish}
+          initialValues={{
+            residence: ['zhejiang', 'hangzhou', 'xihu'],
+            prefix: '86',
+          }}
+          scrollToFirstError
         >
-          <Input />
-        </Form.Item>
+          <Form.Item label="Seller" valuePropName="checked">
+            <Switch />
+          </Form.Item>
 
-        <Form.Item
-          name="password"
-          label="Password"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your password!',
-            },
-          ]}
-          hasFeedback
-        >
-          <Input.Password />
-        </Form.Item>
-
-        <Form.Item
-          name="confirm"
-          label="Confirm Password"
-          dependencies={['password']}
-          hasFeedback
-          rules={[
-            {
-              required: true,
-              message: 'Please confirm your password!',
-            },
-            ({ getFieldValue }) => ({
-              validator(_, value) {
-                if (!value || getFieldValue('password') === value) {
-                  return Promise.resolve();
-                }
-
-                return Promise.reject(new Error('The two passwords that you entered do not match!'));
+          <Form.Item
+            name="email"
+            label="E-mail"
+            rules={[
+              {
+                type: 'email',
+                message: 'The input is not valid E-mail!',
               },
-            }),
-          ]}
-        >
-          <Input.Password />
-        </Form.Item>
+              {
+                required: true,
+                message: 'Please input your E-mail!',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
 
-        <Form.Item
-          name="nickname"
-          label="Nickname"
-          tooltip="What do you want others to see your name?"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your nickname!',
-              whitespace: true,
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
+          <Form.Item
+            name="password"
+            label="Password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!',
+              },
+            ]}
+            hasFeedback
+          >
+            <Input.Password />
+          </Form.Item>
 
-        <Form.Item
-          name="phone"
-          label="Phone Number"
-          rules={[
-            {
-              required: true,
-              message: 'Please input your phone number!',
-            },
-          ]}
-        >
-          <Input
-            addonBefore={prefixSelector}
-          // style={{
-          //   width: '50%',
-          // }}
-          />
-        </Form.Item>
+          <Form.Item
+            name="confirm"
+            label="Confirm Password"
+            dependencies={['password']}
+            hasFeedback
+            rules={[
+              {
+                required: true,
+                message: 'Please confirm your password!',
+              },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue('password') === value) {
+                    return Promise.resolve();
+                  }
 
-        <Form.Item
-          name="agreement"
-          valuePropName="checked"
-          rules={[
-            {
-              validator: (_, value) =>
-                value ? Promise.resolve() : Promise.reject(new Error('Should accept agreement')),
-            },
-          ]}
-          {...tailFormItemLayout}
-        >
-          <Checkbox>
-            I agree to the terms and policies of Multilevel-Association.
-          </Checkbox>
-        </Form.Item>
-        <Form.Item {...tailFormBt}>
-          <Button type="primary" htmlType="submit" >
-            Register
-          </Button>
-        </Form.Item>
-      </Form></div>
+                  return Promise.reject(new Error('The two passwords that you entered do not match!'));
+                },
+              }),
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item
+            name="nickname"
+            label="Nickname"
+            tooltip="What do you want others to see your name?"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your nickname!',
+                whitespace: true,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            name="phone"
+            label="Phone Number"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your phone number!',
+              },
+            ]}
+          >
+            <Input
+              addonBefore={prefixSelector}
+            // style={{
+            //   width: '50%',
+            // }}
+            />
+          </Form.Item>
+
+          <Form.Item
+            name="agreement"
+            valuePropName="checked"
+            rules={[
+              {
+                validator: (_, value) =>
+                  value ? Promise.resolve() : Promise.reject(new Error('Should accept agreement')),
+              },
+            ]}
+            {...tailFormItemLayout}
+          >
+            <Checkbox>
+              I agree to the terms and policies of Multilevel-Association.
+            </Checkbox>
+          </Form.Item>
+          <Form.Item {...tailFormBt}>
+            <Button type="primary" htmlType="submit" >
+              Register
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
   );
 };
 
