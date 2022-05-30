3d0f5ebdb2f45d6d0c82a9c889ff4d0e42c1c701
minhquanhbt <minhquanhbt@gmail.com>
2022-05-30 23:44:02 +0700

    update UX
    create product detail UX

--- src/api/buyerInterface.js
+++ src/api/buyerInterface.js
@@ -1,3 +1,7 @@
 import instance from './axios'
 //get all product v1
 export const listProduct = async () => instance.get('/v1/getAllProduct')
+//get product detail v1
+export const productDetail = async (argument) => instance.post('/v1/getDetailProduct', argument)
+//get review
+export const getReview = async (argument) => instance.post('/v1/getReviewProduct',argument)
