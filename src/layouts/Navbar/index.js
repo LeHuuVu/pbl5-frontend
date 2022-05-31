2aa997d31ab42324954bbe5cfbb02830f1cf28c7
minhquanhbt <minhquanhbt@gmail.com>
2022-05-31 21:09:56 +0700

    Update index.js

--- src/layouts/Navbar/index.js
+++ src/layouts/Navbar/index.js
@@ -5,11 +5,8 @@ import { Menu, Dropdown, Avatar, Input, Space, Button } from 'antd';
 import { ShoppingCartOutlined } from '@ant-design/icons';
 import './style.scss';
 import logo from '../../logo_app.png';
-import { useSelector  } from "react-redux";
-import { selectUserInfo } from "../../store/userSlice";
 
 export default function Navbar() {
-  const userInfo = useSelector(selectUserInfo);
   const { Search } = Input;
   const onSearch = (value) => console.log(value);
   let user = localStorage.getItem('user-info');
@@ -40,7 +37,7 @@ export default function Navbar() {
     </Menu>
   )
   let checkLogin
-  if (userInfo.name == null){
+  if (user == null){
     checkLogin = (
       <div>
         <a href="/Register" style={{padding:10}}> Đăng ký</a>
