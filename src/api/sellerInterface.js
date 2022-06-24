649381a788dbe560f580336e154e66cc74f1dc9d
minhquanhbt <minhquanhbt@gmail.com>
2022-06-24 18:11:04 +0700

    add delete product function

--- src/api/sellerInterface.js
+++ src/api/sellerInterface.js
@@ -4,9 +4,7 @@ export const listSellingProduct = async (argument) => instance.post('/v1/getProd
 //get sellingproduct detail v1
 export const sellingproductDetail = async (argument) => instance.post('/v1/getDetailProduct', argument)
 //delete Product
-export const delSellingProd = async (argument) => instance.post('/v1/delProduct',argument)
-//update product
-export const updateProduct = async (argument) => instance.post('/v1/delProduct',argument)
+export const delSellingProd = async (argument) => instance.post('/v1/deleteProduct',argument)
 //create product
 export const createProduct = async (argument) => instance.post('/v1/createNewProduct',argument)
 //get company
